8e74c4cc6e7958e237d430938a72211b8db17fdf
art kontreras <arturo.kontreras@gmail.com>
2013-10-18 19:30:06 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pugwarriors::Application.config.secret_token = 'd0066e6e40e39c9749bc6d9f4c57494213565d83a9b949c64c2c3102c9df7c3451d8bdd660258204ab1f67367161f37a492b9f076ac49e4f4ee69be9c150d82a'
