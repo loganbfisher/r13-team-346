b649a79573e1b3ce8524515b6f4a53b0c2467dd0
art kontreras <arturo.kontreras@gmail.com>
2013-10-19 10:12:21 -0500

    Add game model.

---
+++ app/models/game.rb
@@ -0,0 +1,14 @@
+class Game
+  include Mongoid::Document
+
+  field :name, type: String
+  field :game_type, type: String
+  field :location, type: String
+  field :city, type: String
+  field :state, type: String
+  field :zip, type: String
+  field :date, type: Date
+  field :time, type: Time
+  field :player_max, type: String
+  field :equipment, type: String
+end
