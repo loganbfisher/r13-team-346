94a0b1778ef1fec8e0e3a6dca27000c703eb9e0a
art kontreras <arturo.kontreras@gmail.com>
2013-11-08 12:58:19 -0600

    Cleanup comments and add ajax for comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment
   include Mongoid::Document
 
   field :comment, type: String
-  field :author_handle, type: String
-  field :author_image, type: String
   field :date_time, type: DateTime
 
   belongs_to :game
